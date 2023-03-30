dc3c33586b5561d795644f63bb7211f2a672a3a9
Habib-Anwar <121849760+Habib-Anwar@users.noreply.github.com>
2023-03-30 14:17:00 +0600

    retrieve added cart

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -14,9 +14,29 @@ const Shop = () => {
     }, []);
 
     useEffect(()=>{
+        console.log('products', products);
         const storedCart = getShoppingCart();
-        console.log(storedCart)
-    },[])
+        const savedCart = [];
+        // console.log(storedCart)
+        // step 1: get id
+        for(const id in storedCart){
+            //step 2: get the product by using id
+            // console.log(id);
+            const addedProduct = products.find(product => product.id 
+                === id);
+                if(addedProduct){
+                // console.log(addedProduct);
+                // step 3: get quantity of the product
+                const quantity = storedCart[id];
+                addedProduct.quantity = quantity;
+                // step 4: add the addedProduct the the saved cart
+                savedCart.push(addedProduct)
+                }
+                console.log('added product', addedProduct);
+        }
+        // step 5: set the cart
+        setCart(savedCart);
+    },[products])
        
     const handleAddToCart = (product) =>{
         // console.log(product)
